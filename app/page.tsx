bb75ec4551a6631edf7f2507749161ce65886a81
shadinmhd <shadinmhd97@gmail.com>
2024-04-04 16:05:20 +0530

    update: added navbar to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
+import Navbar from '@/components/Navbar'
 import React from 'react'
 
 const Home = () => {
 	return (
-		<div>Home</div>
+		<div className='flex flex-col h-screen w-screen'>
+			<Navbar />
+			<div className='w-full h-full'>
+
+			</div>
+		</div>
 	)
 }
 
